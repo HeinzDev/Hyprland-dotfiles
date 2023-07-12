aaf6f13c85ea678d833e75a913e347bef0fbf041
Enzo <102617566+HeinzDev@users.noreply.github.com>
2023-07-12 09:39:46 -0300

    Hyprland exec update❄️

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -20,11 +20,11 @@
     };
   };
 
-  programs = {
+programs = {
     bash = {
       interactiveShellInit = ''
         if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
-           exec  Hyprland
+           WLR_NO_HARDWARE_CURSORS=1 Hyprland #prevents cursor disappear when using Nvidia drivers
         fi
       '';
     };

9a729b79e83f71c291063ca82b5092075815d021
Enzo <102617566+HeinzDev@users.noreply.github.com>
2023-09-08 16:49:37 -0300

    Keyboard service description❄️

--- hosts/desktop/services/default.nix
+++ hosts/desktop/services/default.nix
@@ -2,12 +2,14 @@
 { config, pkgs, ... }:
 
 let
+  # (Workaround)
   hhkbCommand = ''
     sudo /home/enzo/Downloads/evremap/target/release/evremap remap /home/enzo/Downloads/evremap/hhkb.toml
   '';
 
 in
 {
+  #Evremap service to fix my keyboard layout..
   systemd.services.hhkb = {
     description = "HHKB Service";
     serviceConfig.ExecStart = hhkbCommand;

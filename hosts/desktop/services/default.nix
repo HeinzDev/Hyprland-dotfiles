310c82edc6031da4c033495656ad865c55eca8d6
HeinzDev <heinz1dev@gmail.com>
2023-10-07 15:17:46 -0300

    fixed evremap❄️

--- hosts/desktop/services/default.nix
+++ hosts/desktop/services/default.nix
@@ -1,15 +1,14 @@
-
 { config, pkgs, ... }:
 
 let
   # (Workaround)
   hhkbCommand = ''
-sudo /home/enzo/Apps/Files/evremap/target/release/evremap remap /home/enzo/Apps/Files/evremap/hhkb.toml
+    ${pkgs.sudo}/bin/sudo /home/enzo/Apps/Files/evremap/target/release/evremap remap /home/enzo/Apps/Files/evremap/hhkb.toml
   '';
 
 in
 {
-  #Evremap service to fix my keyboard layout..
+    #Evremap service to fix my keyboard layout..
   systemd.services.hhkb = {
     description = "HHKB Service";
     serviceConfig.ExecStart = hhkbCommand;
